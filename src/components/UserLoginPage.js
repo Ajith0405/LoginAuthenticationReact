fef12664fb7fb0aa9b83d0dc312dc6ee351b365b
AJITH S <128063551+Ajith0405@users.noreply.github.com>
2023-10-07 08:04:17 +0530

    second commit

--- src/components/UserLoginPage.js
+++ src/components/UserLoginPage.js
@@ -5,54 +5,56 @@ import { isAunthenticate, logout } from '../Services/Auth';
 import Navbar from './Navbar';
 import { Navigate, useNavigate } from 'react-router-dom';
 
-export default function UserLoginPage ()  {
+export default function UserLoginPage() {
 
     const navigate = useNavigate()
 
-    const [user,setUser] = useState({
-        name:"",
-        email:""
+    const [user, setUser] = useState({
+        name: "",
+        email: ""
     })
 
-    useEffect(()=>{
-        if(isAunthenticate()){
-        UserDetailsApi().then((resp)=>{
-            console.log(resp);
-            setUser({name:resp.data.users[0].displayName,
-            email:resp.data.users[0].email})
-        })
-    }
-    },[])
+    useEffect(() => {
+        if (isAunthenticate()) {
+            UserDetailsApi().then((resp) => {
+                console.log(resp);
+                setUser({
+                    name: resp.data.users[0].displayName,
+                    email: resp.data.users[0].email
+                })
+            })
+        }
+    }, [])
 
-    const LogoutUser=()=>{
+    const LogoutUser = () => {
         logout();
         navigate('/login')
-        
+
     }
-    if(!isAunthenticate()){
-        return <Navigate to="/login"/>
+    if (!isAunthenticate()) {
+        return <Navigate to="/login" />
     }
-  return (
-    <div>
+    return (
+        <div>
             <Navbar LogoutUser={LogoutUser} />
             <div className='text-center'>
                 <h3>User Dashboard</h3>
             </div>
-            {user.name && user.email ?( 
-                <div className='position-absolute top-50 start-50 translate-middle'> 
-                 <div className="card w-100 pt-3 mx-auto" style={{color:'white', backgroundColor:'gray'}} >
-                <img className="card-img-top mx-auto" src={img} alt="Card" style={{width:'120px'}} />
-                <div className="card-body">
-                    <p>Hi Welcome ! </p>
-                    <h4 className="card-title">{user.name}</h4>
-                    <p className="card-text">Email : {user.email}</p>
-                    
+            {user.name && user.email ? (
+                <div className='position-absolute top-50 start-50 translate-middle'>
+                    <div className="card w-100 pt-3 mx-auto" style={{ color: 'white', backgroundColor: 'gray',boxShadow:'rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px' }} >
+                        <img className="card-img-top mx-auto" src={img} alt="Card" style={{ width: '120px' }} />
+                        <div className="card-body text-center">
+                            <p>Hi Welcome ! </p>
+                            <h4 className="card-title">{user.name}</h4>
+                            <p className="card-text">Email : {user.email}</p>
+
+                        </div>
+                    </div>
                 </div>
-                </div>
-                </div>
-           ):null}
-        
-    </div>
-  )
+            ) : null}
+
+        </div>
+    )
 }
 
